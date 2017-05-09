8daaa78c0a9c1bd691bba361c705e1793bdbf9cb
odessitka <odessitka@list.ru>
2017-05-09 21:22:57 +0100

    initial commit

--- Distances/views.py
+++ Distances/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from Distances.utils import daft_search
+
 
 def index(request):
     return HttpResponse("Hello, world. Here you will find useful info soon")
 # Create your views here.
+
+
+def grabdata(request):
+    daft_search.main()
+    return  HttpResponse("data grabbed!!!")
