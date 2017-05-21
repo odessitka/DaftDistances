92ca8158f23c0bcf14986eaba2e1651471e1f1bb
ppnevmatikatos <pnevmap@gmail.com>
2017-05-21 13:37:43 +0100

    add cloud support

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    url('', admin.site.urls),
     url(r'^distances/', include('Distances.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
